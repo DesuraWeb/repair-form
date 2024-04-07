4b6bf3ddb465f7d13fa55e66652c0a938e1d43b7
DesuraWeb <contact@desura.fr>
2024-04-07 18:02:45 +0200

    Update 1.1
    Rangement
    Ajout fonctionnalité
    Ajout css

--- js/app.js
+++ js/app.js
@@ -1,10 +1,12 @@
 document.addEventListener('DOMContentLoaded', function() {
     var lock = null; // Instance of PatternLock
 
-    document.getElementById('unlockType').addEventListener('change', function() {
-        var unlockType = this.value;
+    // Fonction pour mettre à jour l'affichage en fonction du type de déverrouillage sélectionné
+    function updateUnlockDisplay() {
+        var unlockType = document.getElementById('unlockType').value;
         var pinCode = document.getElementById('pinCode');
         var patternContainer = document.getElementById('patternContainer');
+
         if (unlockType === 'pin') {
             pinCode.style.display = 'block';
             patternContainer.style.display = 'none';
@@ -16,21 +18,27 @@ document.addEventListener('DOMContentLoaded', function() {
             pinCode.style.display = 'none';
             patternContainer.style.display = 'block';
             if (!lock) {
-                lock = new PatternLock('#lock', { // Initialize on SVG element directly
+                lock = new PatternLock('#lock', {
                     onPattern: function(pattern) {
-                        // Optionally update a hidden field with the drawn pattern
-                        console.log(pattern);
+                        console.log(pattern); // Optionally update a hidden field with the drawn pattern
                     }
                 });
             }
         }
-    });
+    }
+
+    // Initialiser l'affichage des champs lors du chargement de la page
+    updateUnlockDisplay();
+
+    // Écouter les changements sur le menu déroulant 'unlockType'
+    document.getElementById('unlockType').addEventListener('change', updateUnlockDisplay);
 
     document.getElementById('repairForm').addEventListener('submit', function(event) {
         event.preventDefault();
         const fullname = document.getElementById('fullname').value;
         const email = document.getElementById('email').value;
         let unlockCode = '';
+
         if (document.getElementById('unlockType').value === 'pattern' && lock) {
             unlockCode = lock.getPattern();
         } else {
@@ -38,6 +46,15 @@ document.addEventListener('DOMContentLoaded', function() {
         }
 
         console.log(`Dossier pour ${fullname}, Email: ${email}, Code: ${unlockCode}`);
-        // Add logic here to send the information to the server or process it locally.
+        // Ici, ajoutez la logique pour envoyer les informations au serveur ou les traiter localement.
     });
 });
+
+// Gestion de la popup d'informations
+function openPopup() {
+    document.getElementById('infoPopup').style.display = 'flex';
+}
+
+function closePopup() {
+    document.getElementById('infoPopup').style.display = 'none';
+}
